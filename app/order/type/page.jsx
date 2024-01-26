97fc1897e749a2d39db1287c2883839bd04db574
Rhys <rhyswoody@gmail.com>
2024-01-26 12:33:49 -0800

    Add new logo assets

--- app/order/type/page.jsx
+++ app/order/type/page.jsx
@@ -1,71 +1,64 @@
 'use client';
+import React, { useEffect } from 'react';
 import { useOrderStore } from 'utils/state/store/Order.js';
 
 const Type = () => {
     const order = useOrderStore(state => state.order);
     const setField = useOrderStore(state => state.setField);
-    const renderComponent = (selectedOption) => {
-        switch (selectedOption) {
-          case 'All In One':
-            return <AllInOne />;
-          case 'Blank Cans':
-            return <BlankCans  />;
-          case 'Can App':
-            return <CanApp />;
-          case 'Labels Only':
-            return <LabelsOnly />;
-          case 'Supplies':
-            return <Supplies />;
-          default:
-            return null;
-        }
-      };
-    const handleClick = (e) => {
-        e.preventDefault()
-        setField('orderType', e.target.value)
-        renderComponent(e.target.value)
-        console.log(order)
+    useEffect(() => {
+        console.log(order);
+    }, [order]);
+
+    const handleClick = (value) => {
+        setField('orderType', value)
     }
     return (
         <div className="flex justify-center items-center min-h-screen pb-64">
             <div className="grid md:grid-cols-3 gap-4 justify-items-center items-center px-40 md:px-14">
                 <div
-                    onClick={handleClick}
+                    onClick={() => handleClick("All In One")}
                     value="All In One"
-                    className="w-full h-36 rounded-lg px-4 py-4 m-2 flex items-center justify-center flex-col text-white hover:text-yellow-500 hover:bg-white cursor-pointer hover:bg-opacity-50"
+                    className="w-full h-62 rounded-lg px-4 py-4 m-2 flex items-center justify-center flex-col text-white hover:text-yellow-500 hover:bg-white cursor-pointer hover:bg-opacity-50"
                 >
-                    <h1>All In One</h1>
+                    <img src="/assets/icons/allinonelogo.svg" alt="All In One Icon" className="w-24 h-24 mb-2" />
+                    <h1 className='font-barlowbold text-2xl'>ALL IN ONE</h1>
                     <div className="vessel_sub_text text-center px-4 py-4">Order your label, can and application in one</div>
                 </div>
                 <div
-                    onClick={handleClick}
+                    onClick={() => handleClick("Labels Only")}
                     value="Labels Only"
-                    className="w-full h-36 rounded-lg px-4 py-4 m-2 flex items-center justify-center flex-col  text-white hover:text-yellow-500 hover:bg-white cursor-pointer hover:bg-opacity-50"
+                    className="w-full h-62 rounded-lg px-4 py-4 m-2 flex items-center justify-center flex-col  text-white hover:text-yellow-500 hover:bg-white cursor-pointer hover:bg-opacity-50"
                 >
-                    <div>Labels Only</div>
-                    <div className="vessel_sub_text text-center px-4 py-4">Order flexographic Shrink Sleeves</div>
+                    <img src="/assets/icons/labelsicon.svg" alt="Labels only Icon" className="w-32 h-32" />
+                    <h1 className='font-barlowbold text-2xl'>LABELS ONLY</h1>
+                    <div className="vessel_sub_text text-center px-6 py-4 pb-10">Order flexographic Shrink Sleeves only</div>
                 </div>
                 <div
-                    onClick={handleClick}
+                    onClick={() => handleClick("Can App")}
                     value="Can App"
-                    className="w-full h-36 rounded-lg px-4 py-4 m-2 flex items-center justify-center flex-col  text-white hover:text-yellow-500 hover:bg-white cursor-pointer hover:bg-opacity-50"
+                    className="w-full h-62 rounded-lg px-4 py-4 m-2 flex items-center justify-center flex-col  text-white hover:text-yellow-500 hover:bg-white cursor-pointer hover:bg-opacity-50"
                 >
-                    <div>Can App</div>
+                    <img src="/assets/icons/canapplogo.png" alt="Can App Icon" className="w-24 h-24 mb-2" />
+                    <h1 className='font-barlowbold text-2xl'>CAN + APPLICATION</h1>
                     <div className="vessel_sub_text text-center px-4 py-4">Book application of customer-owned labels</div>
                 </div>
                 <div
-                    onClick={handleClick}
+                    onClick={() => handleClick("Blank Cans")}
                     value="Blank Cans"
-                    className="w-full h-36 rounded-lg px-4 py-4 m-2 flex items-center justify-center flex-col text-white hover:text-yellow-500 hover:bg-white cursor-pointer hover:bg-opacity-50"
+                    className="w-full h-62 rounded-lg px-4 py-4 m-2 flex items-center justify-center flex-col text-white hover:text-yellow-500 hover:bg-white cursor-pointer hover:bg-opacity-50"
                 >
-                    <div>Blank Cans</div>
+                    <img src="/assets/icons/blankcanslogo.png" alt="Blank Cans Icon" className="w-24 h-24 mb-2" />
+                    <h1 className='font-barlowbold text-2xl'>BLANK CANS</h1>
+                    <div className="vessel_sub_text text-center px-4 py-4">Order LTL Bite Cans</div>
                 </div>
                 <div
-                    onClick={handleClick}
+                    onClick={() => handleClick("Supplies Only")}
                     value="Supplies"
-                    className="w-full h-36 rounded-lg px-4 py-4 m-2 flex items-center justify-center flex-col  text-white hover:text-yellow-500 hover:bg-white cursor-pointer hover:bg-opacity-50"
+                    className="w-full h-62 rounded-lg px-4 py-4 m-2 flex items-center justify-center flex-col  text-white hover:text-yellow-500 hover:bg-white cursor-pointer hover:bg-opacity-50"
                 >
-                    <div>Supplies</div>
+                    <img src="/assets/icons/supplieslogo.svg" alt="Supplies Icon" className="w-24 h-24 mb-2" />
+                    <h1 className='font-barlowbold text-2xl'>SUPPLIES</h1>
+                    <div className="vessel_sub_text text-center px-4 py-4">Order Supplies Only</div>
                 </div>
             </div>
         </div>
