14dac84bf1ad2ccd665bfce2a0a2803137b9caab
Rhys <rhyswoody@gmail.com>
2024-01-30 14:13:29 -0800

    add allinone reorder

--- app/order/allinone/reorder/page.jsx
+++ app/order/allinone/reorder/page.jsx
@@ -1,9 +1,114 @@
-import React from 'react'
+"use client";
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { useOrderStore } from 'utils/state/store/Order.js';
+import PO from '../../../../components/forms/inputs/PO';
+import ApplicationType from '../../../../components/forms/inputs/ApplicationType';
+import CanSize from '../../../../components/forms/inputs/CanSize';
+import Brand from '../../../../components/forms/inputs/Brand';
+import CansCalculated from '../../../../components/forms/formSections/CansCalculated';
+import SuppliesSection from '../../../../components/forms/formSections/SuppliesSection';
+import ShippingDetails from '../../../../components/forms/formSections/ShippingDetails';
+import AddressInfo from '../../../../components/forms/formSections/AddressInfo';
+import CopackerEmail from '../../../../components/forms/inputs/CopackerEmail';
+import Comments from '../../../../components/forms/inputs/Comments';
 
 const AllInOneReorder = () => {
-  return (
-    <div>Allinone</div>
-  )
-}
+  const router = useRouter();
+  const [submitting, setSubmitting] = useState(false);
+  const order = useOrderStore(state => state.order);
+  const setField = useOrderStore(state => state.setField);
 
-export default AllInOneReorder
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+  
+    const url = process.env.zapier_URL;
+    setSubmitting(true);
+  
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(order),
+      });
+  
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+  
+      // Do something with the response if needed
+  
+    } catch (error) {
+      console.error('There was a problem with the fetch operation: ', error);
+    } finally {
+      setSubmitting(false);
+    }
+  };  
+  
+
+return (
+  <section className="flex-start flex-col w-11/12 max-w-full bg-vp-orchid rounded-lg p-24 small_scrn_less_padding my-24 mx-60">
+    <h1 className="head_text text-left">
+      <span className="text-vp-yellow">All In One</span>
+    </h1>
+    <form onSubmit={handleSubmit} className="mt-10 mb-10 w-full max-w-2xl mx-auto flex flex-col gap-7">
+    <div className="flex mb-4 flex-column-below-900">
+      <div className='w-1/2 width-100-below-900'>
+      <PO />
+      </div>
+      <div className='w-1/2 width-100-below-900'>
+      <CanSize/>
+      </div>
+    </div>
+
+    <div className="flex mb-4 flex-column-below-900">
+    <div className='w-1/2 width-100-below-900'>
+      <Brand />
+      </div>
+      <div className='w-1/2 width-100-below-900'>
+      <ApplicationType />
+      </div>
+    </div>
+    <div>
+      <CansCalculated />
+    </div>
+    <div>
+      <SuppliesSection soleSupply={false} />
+    </div>
+
+    <div>
+      <ShippingDetails />
+    </div>
+
+    <div className="flex mb-4 flex-column-below-900 bg-grey-below-900">
+      <div className="w-1/2 mr-8 width-100-below-900">
+        <AddressInfo />
+      </div>
+      <div className="w-1/2 width-100-below-900">
+        <CopackerEmail />
+      </div>
+    </div>
+
+    <div>
+      <Comments />
+    </div>
+      
+      
+      <div className="flex-end mx-3 mb-5 gap-4">
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          disabled={submitting}
+        >
+          {submitting ? 'Submitting...' : 'Submit'}
+        </button>
+      </div>
+    </form>
+  </section>
+);
+};
+
+export default AllInOneReorder;
+
