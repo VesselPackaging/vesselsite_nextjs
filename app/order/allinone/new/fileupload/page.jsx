40342b851f6371ca7af1818026208c0243c8f77a
Rhys <rhyswoody@gmail.com>
2024-02-06 11:45:23 -0800

    add in fileupload page and add styling

---
+++ app/order/allinone/new/fileupload/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import FileUpload from '../../../../../components/forms/inputs/FileUpload'
+
+const FileUploadSubmit = () => {
+  return (
+    <FileUpload />
+  )
+}
+
+export default FileUploadSubmit
--- components/forms/inputs/FileUpload.jsx
+++ components/forms/inputs/FileUpload.jsx
@@ -4,7 +4,8 @@ import { useOrderStore } from 'utils/state/store/Order.js';
 
 const FileUpload = (customerInfo) => {
   const [file, setFile] = useState(null);
-  const [isSubmitDisabled, setIsSubmitDisabled] = useState(false);
+  const [fileName, setFileName] = useState('');
+  const [isSubmitDisabled, setIsSubmitDisabled] = useState(true);
   const order = useOrderStore((state) => state.order);
   const { companyName, brand } = order;
 
@@ -29,38 +30,63 @@ const FileUpload = (customerInfo) => {
     }
   };
   return (
-    <form>
-      <input
-        type="file"
-        name="file"
-        onChange={(e) => {
-          const file = e.target.files[0];
-          const sizeInMB = file.size / (1024 * 1024);
-          const extension = file.name.split('.').pop().toLowerCase();
-          const acceptableExtensions = ['ai', 'pdf'];
+    <section className="flex flex-col items-center w-11/12 max-w-full bg-vp-orchid rounded-lg p-24 small_scrn_less_padding my-24 mx-60">
+      <h1 className="head_text text-left">
+        <span className="text-vp-yellow">File Upload & Submit</span>
+      </h1>
+      <form className="w-full flex flex-col items-center">
+        <div className="mt-24 space-y-6">
+          <label
+            htmlFor="file"
+            className="py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-vp-black hover:bg-vp-copper focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 cursor-pointer"
+          >
+            Select File
+          </label>
+          <input
+            id="file"
+            type="file"
+            name="file"
+            className="hidden"
+            onChange={(e) => {
+              if (e.target.files.length === 0) {
+                setFile(null);
+                setIsSubmitDisabled(true);
+                return;
+              }
 
-          if (sizeInMB > 150) {
-            alert('File size exceeds 150MB. Please select a smaller file.');
-            setFile(null);
-            setIsSubmitDisabled(true);
-          } else if (!acceptableExtensions.includes(extension)) {
-            alert('Invalid file type. Please select a .ai or .pdf file.');
-            setFile(null);
-            setIsSubmitDisabled(true);
-          } else {
-            setFile(file);
-            setIsSubmitDisabled(false);
-          }
-        }}
-      />
-      <button
-        type="submit"
-        disabled={isSubmitDisabled}
-        className={`group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white ${isSubmitDisabled ? 'bg-gray-500' : 'bg-vp-yellow hover:bg-vp-green focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'}`}
-      >
-        Upload
-      </button>{' '}
-    </form>
+              const file = e.target.files[0];
+              const sizeInMB = file.size / (1024 * 1024);
+              const extension = file.name.split('.').pop().toLowerCase();
+              const acceptableExtensions = ['ai', 'pdf'];
+
+              if (sizeInMB > 150) {
+                alert('File size exceeds 150MB. Please select a smaller file.');
+                setFile(null);
+                setIsSubmitDisabled(true);
+              } else if (!acceptableExtensions.includes(extension)) {
+                alert('Invalid file type. Please select a .ai or .pdf file.');
+                setFile(null);
+                setIsSubmitDisabled(true);
+              } else {
+                setFile(file);
+                setFileName(file.name);
+                setIsSubmitDisabled(false);
+              }
+            }}
+          />
+          {fileName && <p className="mt-2 text-center">{fileName}</p>}
+
+          <button
+            type="submit"
+            onClick={onSubmit}
+            disabled={isSubmitDisabled}
+            className={`w-full group relative flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white ${isSubmitDisabled ? 'bg-gray-500' : 'bg-vp-yellow hover:bg-vp-green focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'}`}
+          >
+            Upload
+          </button>
+        </div>
+      </form>
+    </section>
   );
 };
 
