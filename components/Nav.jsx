97c68780b1b4e7aaf825d879eb5b86a01813dc5c
Rhys <rhyswoody@gmail.com>
2024-02-12 14:31:56 -0800

    update nav link to /

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 const Nav = () => {
   return (
     <nav className='flex-between w-full mb-2 pt-3 relative'>
-      <Link href="/order" className='flex gap-2'>
+      <Link href="/" className='flex gap-2'>
         <Image 
           src="/assets/vessellogo.svg" 
           alt="vessel logo"
