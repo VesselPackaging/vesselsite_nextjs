d3e0b4afc3d95bbb271c2a06de0421e6adb250e0
Rhys <rhyswoody@gmail.com>
2024-03-01 12:41:01 -0800

    add dropdown for leadtime location

--- app/leadtimes/page.jsx
+++ app/leadtimes/page.jsx
@@ -1,8 +1,31 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 
 const LeadTimes = () => {
+    const [location, setLocation] = useState('');
+
   return (
     <div className="flex flex-col items-center justify-center space-y-4 min-h-screen pb-64">
+      <div className="w-full px-3 items-center">
+        <label
+          className="uppercase tracking-wide text-white text-xs font-bold mb-2 flex justify-center"
+          htmlFor="location"
+        >
+          Select Location
+        </label>
+        <div className="flex justify-center">
+        <select
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+          className="appearance-none block w-1/2 bg-gray-200 text-vp-yellow border rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white text-center"
+          >
+          <option value="">Select a location</option>
+          <option value="Vancouver">Vancouver</option>
+          <option value="Calgary">Calgary</option>
+          <option value="Mississauga">Mississauga</option>
+        </select>
+        </div>
+      </div>
       <div className="flex justify-center items-center">
         <div className="grid md:grid-cols-3 gap-4 justify-items-center items-center px-40 md:px-14">
           <div className="max-w-md w-full space-y-4 p-6 bg-white rounded-xl shadow-md">
@@ -52,9 +75,7 @@ const LeadTimes = () => {
           </div>
         </div>
       </div>
-      <div className="self-center text-white">
-        ** Business Days
-      </div>
+      <div className="self-center text-white">** Business Days</div>
       <div className="self-center text-white">
         *new all-in-one SKUs lead time vary, average +7 days
       </div>
