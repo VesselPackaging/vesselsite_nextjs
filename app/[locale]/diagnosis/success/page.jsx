1c4e2b1edc11815f66d80caddabb8c467e8cbe5b
Rhys <rhyswoody@gmail.com>
2024-04-11 10:17:32 -0700

    add french for order diagnosis

--- app/[locale]/diagnosis/success/page.jsx
+++ app/[locale]/diagnosis/success/page.jsx
@@ -2,30 +2,14 @@
 import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { useOrderStore } from '../../../../utils/state/store/Order';
+import { useTranslations } from 'next-intl';
 
 const SuccessOrder = ({ params: { locale } }) => {
   const router = useRouter();
   const { setField } = useOrderStore();
+  const t = useTranslations('Index');
 
   const handleResetAndRedirect = () => {
-    setField('newOrReorder', '');
-    setField('canSize', '');
-    setField('brand', '');
-    setField('application', '');
-    setField('numberOfCans', '');
-    setField('endType', '');
-    setField('numberOfSleeves', '');
-    setField('pakTechType', '');
-    setField('numberOfBoxes', '');
-    setField('trayType', '');
-    setField('bundlesofTrays', '');
-    setField('address', '');
-    setField('deliveryMethod', '');
-    setField('dunnageType', '');
-    setField('date', '');
-    setField('copackerEmail', '');
-    setField('comments', '');
-    setField('file', '');
     router.push(`/${locale}/type`);
   };
 
@@ -33,16 +17,14 @@ const SuccessOrder = ({ params: { locale } }) => {
     <div className="flex items-center justify-center min-h-screen py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8 p-6 bg-white rounded-xl shadow-md">
         <h1 className="head_text text-center">
-          <span className="text-vp-green">Your Order has been recieved!</span>
+          <span className="text-vp-green">{t('orderSuccess')}</span>
         </h1>
-        <p className="text-vp-black text-center">
-          Thank you for choosing Vessel, we will be in touch with you shortly.
-        </p>
+        <p className="text-vp-black text-center">{t('orderSuccessSubtext')}</p>
         <button
           className="w-full group relative flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-vp-yellow hover:bg-vp-green focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           onClick={handleResetAndRedirect}
         >
-          Add Another Order
+          {t('addAnother')}
         </button>
       </div>
     </div>
--- app/[locale]/diagnosis/unsuccessful/page.jsx
+++ app/[locale]/diagnosis/unsuccessful/page.jsx
@@ -2,30 +2,14 @@
 import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { useOrderStore } from '../../../../utils/state/store/Order';
+import { useTranslations } from 'next-intl';
 
 const SuccessOrder = ({ params: { locale } }) => {
   const router = useRouter();
   const { setField } = useOrderStore();
+  const t = useTranslations('Index');
 
   const handleResetAndRedirect = () => {
-    setField('newOrReorder', '');
-    setField('canSize', '');
-    setField('brand', '');
-    setField('application', '');
-    setField('numberOfCans', '');
-    setField('endType', '');
-    setField('numberOfSleeves', '');
-    setField('pakTechType', '');
-    setField('numberOfBoxes', '');
-    setField('trayType', '');
-    setField('bundlesofTrays', '');
-    setField('address', '');
-    setField('deliveryMethod', '');
-    setField('dunnageType', '');
-    setField('date', '');
-    setField('copackerEmail', '');
-    setField('comments', '');
-    setField('file', '');
     router.push(`/${locale}/type`);
   };
 
@@ -33,17 +17,14 @@ const SuccessOrder = ({ params: { locale } }) => {
     <div className="flex items-center justify-center min-h-screen py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8 p-6 bg-white rounded-xl shadow-md">
         <h1 className="head_text text-center">
-          <span className="text-vp-red">Something went wrong</span>
+          <span className="text-vp-red">{t('orderFailure')}</span>
         </h1>
-        <p className="text-vp-black text-center">
-          Sorry about that, please try again. If the problem persists, please
-          contact your Vessel Rep.
-        </p>
+        <p className="text-vp-black text-center">{t('orderFailureSubtext')}</p>
         <button
           className="w-full group relative flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-vp-black hover:bg-vp-yellow focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           onClick={handleResetAndRedirect}
         >
-          Try Again
+          {t('tryAgain')}
         </button>
       </div>
     </div>
