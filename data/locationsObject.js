5aefc57f71d8db31b172a2257be79d11d33b4dc2
Rhys <rhyswoody@gmail.com>
2024-04-12 08:35:26 -0700

    update locations object

--- data/locationsObject.js
+++ data/locationsObject.js
@@ -490,8 +490,6 @@ const locations = {
           ['200 B64 (250 ml Slim Cans Only)', 600],
           ['202 SuperEnd REC Epoxy', 624],
           ['202 LOE REC Epoxy', 608],
-          ['202 LOE QC5 Epoxy', 608],
-          ['202 LOE QC20 Epoxy', 608],
           ['202 LOE 10 STATE USA BPANI', 608],
         ],
       },
