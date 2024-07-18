855b232e8c0ac6ab1aa16416fc6314e791f9cebe
Rhys <rhyswoody@gmail.com>
2024-07-18 14:03:06 -0700

    finish list of faq questions

--- app/[locale]/faq/page.jsx
+++ app/[locale]/faq/page.jsx
@@ -119,16 +119,104 @@ const faqData = [
     question: 'How many colours can I have in my artwork?',
     answer: (
       <>
-        For flexo, we can print using up to 7 plates. Please take into account
-        that a plate will need to be used for each spot/process colour, matte
-        varnish, white layer. If printed digitally, there is no limit on
-        colours. However, PMS spot colours will be converted to their CMYK
-        equivalents. Here are the details:
-        <ul className="list-disc list-inside mt-2">
-          <li>Spot/process colour</li>
-          <li>Matte varnish</li>
-          <li>White layer</li>
+        <ul className="list-disc list-outside ml-6 mt-2">
+          <li>
+            For flexo, we can print using up to 7 plates. Please take into
+            account that a plate will need to be used for each spot/process
+            colour, matte varnish, white layer.
+          </li>
+          <li>
+            {' '}
+            If printed digitally, there is no limit on colours. However, PMS
+            spot colours will be converted to their CMYK equivalents.
+          </li>
         </ul>
+      </>
+    ),
+  },
+  {
+    question: 'Should I use Pantone colours or CMYK?',
+    answer: (
+      <>
+        <ul className="list-disc list-outside ml-6 mt-2">
+          <li>
+            You can use both for flexographic printing. Please use the Pantone
+            Solid Coated reference when choosing your spot colours.
+          </li>
+          <li>
+            {' '}
+            Digital print labels are printed using CMYK process colours. Pantone
+            colours colours will be converted to their closest CMYK equivalents.
+            Extended colour gamut printing is available for an additional
+            charge.
+          </li>
+        </ul>
+      </>
+    ),
+  },
+  {
+    question:
+      'I have labels/printed cans from another supplier. Can you colour match them?',
+    answer: (
+      <>
+        Yes! If you have an old printed can or labeled can, we can arrange to
+        have a sample sent to our print partners to colour match. Please allow a
+        little time for transit, and the matching process. It is unlikely to
+        achieve a 100% match, but our printers will match them as closely as
+        possible.
+      </>
+    ),
+  },
+  {
+    question: 'Why does my barcode have to be turned vertically?',
+    answer: (
+      <>
+        Printing the barcode vertically ensures scannability. Horizontal
+        distortion from the shrink sleeve process and curvature of the can may
+        render a horizontal barcode unscannable.
+      </>
+    ),
+  },
+  {
+    question: 'How do I get a metallic look on my labels?',
+    answer: (
+      <>
+        To get an opaque colour, white ink is printed behind the colours.To make
+        any element appear metallic, the white layer can have cutouts to allow
+        the metal of the can/PSL to show through. This will make colours appear
+        metallic.
+      </>
+    ),
+  },
+  {
+    question: 'Can I have the can show through the sleeve?',
+    answer: (
+      <>
+        Yes. Similar to the process for the metallic look, he white layer may
+        have cutouts to allow the metal of the can to show through.
+      </>
+    ),
+  },
+  {
+    question: 'Why is there a coloured outline around my white text?',
+    answer: (
+      <>
+        For legibility, white text is outlined when printed in CMYK (flexo). If
+        your white text is on a spot colour background, there is no need for an
+        additional outline.
+      </>
+    ),
+  },
+  {
+    question:
+      'I want a matte varnish on my sleeves. Why should it only go to the bottom of the neck?',
+    answer: (
+      <>
+        Although the chemical is approved for packaging, it is not approved by
+        the FDA or Health Canada for food consumption. If you do want to have
+        the matte varnish go to the top, we would need to have your choice in
+        writing and any varnish related issues will not be the responsibility of
+        Vessel or our print partners.
       </>
     ),
   },
