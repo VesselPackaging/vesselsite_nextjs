22f9dbf78be85b7034b092a8a905cc36744174a2
Rhys <rhyswoody@gmail.com>
2024-07-26 10:30:57 -0700

    continue artwork resources page

--- app/[locale]/artwork-resources/page.jsx
+++ app/[locale]/artwork-resources/page.jsx
@@ -8,11 +8,26 @@ const ArtworkResources = () => {
       <div>
         <div className="">
           <div className="flex flex-col gap-6 ml-6">
-            <h2>PRESSURE SENSITIVE LABELS (PSL)</h2>
+            <div className="flex flex-row items-center">
+              <img
+                src="/assets/PSL_icon.svg"
+                height={62}
+                width={62}
+                alt="pslLogo"
+                className="bg-tb-turq rounded-md p-1"
+              />
+              <h2 className="ml-3 text-3xl font-barlowbold leading-11 text-tb-violet">
+                PRESSURE SENSITIVE LABELS (PSL)
+              </h2>
+            </div>
             <p>
               Standard size templates that match our standard pricing. For
               custom sizes and Recyc-Québec paper PSLs, please contact
-              gfx@vesselpackaging.com.
+              <a className="text-tb-turq" href="mailto:gfx@vesselpackaging.com">
+                {' '}
+                gfx@vesselpackaging.com
+              </a>
+              .
             </p>
           </div>
           <div className="selection_wrapper">
@@ -51,39 +66,83 @@ const ArtworkResources = () => {
 
         <div className="">
           <div className="flex flex-col gap-6 ml-6">
-            <h2>SHRINK SLEEVING (SS)</h2>
+            <div className="flex flex-row items-center">
+              <img
+                src="/assets/SS_icon.svg"
+                height={62}
+                width={62}
+                alt="pslLogo"
+                className="bg-tb-orange rounded-md p-1"
+              />
+              <h2 className="ml-3 text-3xl font-barlowbold leading-11 text-tb-violet">
+                SHRINK SLEEVING (SS))
+              </h2>
+            </div>
             <p>
-            Our shrink sleeve templates can help you lay out your artwork 
-            so the important stuff doesn't get distorted by the curved can
-            areas and seams.
+              Our shrink sleeve templates can help you lay out your artwork so
+              the important stuff doesn't get distorted by the curved can areas
+              and seams.
             </p>
           </div>
           <div className="selection_wrapper">
             <div className="p-12 flex flex-row gap-6">
               <BoxComponent
                 title="355ml STD"
-                rect1Text='Get 355ml STD'
+                rect1Text="Get 355ml STD"
                 rect1Link="https://example.com/1"
-                rectColor="bg-vp-yellow"
+                rectColor="bg-tb-orange"
               />
               <BoxComponent
                 title="473ml STD"
-                rect1Text='Get 473ml STD'
+                rect1Text="Get 473ml STD"
                 rect1Link="https://example.com/1"
-                rectColor="bg-vp-yellow"
+                rectColor="bg-tb-orange"
               />
               <BoxComponent
                 title="355ml SLK"
-                rect1Text='Get 355ml SLK'
+                rect1Text="Get 355ml SLK"
                 rect1Link="https://example.com/1"
-                rectColor="bg-vp-yellow"
+                rectColor="bg-tb-orange"
               />
               <BoxComponent
                 title="250ml SLM"
-                rect1Text='Get 250ml SLM'
+                rect1Text="Get 250ml SLM"
                 rect1Link="https://example.com/1"
-                rectColor="bg-vp-yellow"
+                rectColor="bg-tb-orange"
               />
+            </div>
+          </div>
+        </div>
+
+        <div className="">
+          <div className="flex flex-col gap-6 ml-6">
+            <div className="flex flex-row items-center">
+              <img
+                src="/assets/SS_icon.svg"
+                height={62}
+                width={62}
+                alt="pslLogo"
+                className="bg-tb-pink rounded-md p-1"
+              />
+              <h2 className="ml-3 text-3xl font-barlowbold leading-11 text-tb-violet">
+                PRESS PRINTED CANS
+              </h2>
+            </div>
+            <p>
+              Resources for press printed can art are available directly from
+              Crown&trade;.
+            </p>
+            <div className="flex justify-center">
+              <a
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://www.crowncork.com/beverage-packaging/design-support/artwork-templates"
+                className="bg-tb-pink rounded-md w-9/12 p-1"
+              >
+                <h3 className="flex items-center justify-center t">
+                  Go to Crown&trade; resources
+                </h3>
+              </a>
             </div>
           </div>
         </div>
